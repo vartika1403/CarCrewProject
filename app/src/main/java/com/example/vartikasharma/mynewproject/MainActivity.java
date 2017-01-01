2a5a92942e9c1e2b0391bd86c682e57be2f43b81
vartika1403 <vartika.1403@gmail.com>
2017-01-02 02:41:18 +0530

    api call response

--- app/src/main/java/com/example/vartikasharma/mynewproject/MainActivity.java
+++ app/src/main/java/com/example/vartikasharma/mynewproject/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.vartikasharma.mynewproject;
 
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.View;
 
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.squareup.okhttp.HttpUrl;
 import com.squareup.okhttp.OkHttpClient;
 
@@ -17,15 +17,19 @@ public class MainActivity extends AppCompatActivity {
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
     private OkHttpClient client;
     private String url;
+    private Gson gson;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         client = new OkHttpClient();
+        gson = new GsonBuilder().create();
 
-        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api.openweathermap.org/data/2.5/weather?").newBuilder();
-        urlBuilder.addQueryParameter("q", "London");
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("http://api.openweathermap.org/data/2.5/forecast/city?id=524901").newBuilder();
+        urlBuilder.addQueryParameter("APPID", "ed34795f35c87eb45c31e75d6b56ea43");
+
         url = urlBuilder.build().toString();
+        Log.i(LOG_TAG, "the url, " + url);
 
         loadContent();
     }
