dfd0a82b2feef6a3110dade46876c6eaaefc2a8f
vartika1403 <vartika.1403@gmail.com>
2017-01-02 00:59:50 +0530

    gradle changes

--- app/src/main/java/com/example/vartikasharma/mynewproject/MainActivity.java
+++ app/src/main/java/com/example/vartikasharma/mynewproject/MainActivity.java
@@ -1,28 +1,48 @@
 package com.example.vartikasharma.mynewproject;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+
+import com.squareup.okhttp.HttpUrl;
+import com.squareup.okhttp.OkHttpClient;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
     private OkHttpClient client;
-    private String url ;
+    private String url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        client = new OkHttpClient();
 
-    // Start the service
-    public void startService(View view) {
-        startService(new Intent(this, DownloadService.class));
-    }
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api.openweathermap.org/data/2.5/weather?").newBuilder();
+        urlBuilder.addQueryParameter("q", "London");
+        url = urlBuilder.build().toString();
 
-    // Stop the service
-    public void stopService(View view) {
-        stopService(new Intent(this, DownloadService.class));
+        loadContent();
+    }
+    private void loadContent() {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    String response = ApiCall.GET(client, url);
+                    //Parse the response string here
+                    Log.d("Response", response);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }.execute();
     }
 }
+
