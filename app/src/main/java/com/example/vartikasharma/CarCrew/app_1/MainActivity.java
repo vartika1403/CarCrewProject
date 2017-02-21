fb332e7c63525eb7ac6a88e2e673e8daef6cd54d
vartika1403 <vartika.1403@gmail.com>
2017-02-22 04:28:30 +0530

    added data to firebase

--- app/src/main/java/com/example/vartikasharma/CarCrew/app_1/MainActivity.java
+++ app/src/main/java/com/example/vartikasharma/CarCrew/app_1/MainActivity.java
@@ -2,15 +2,113 @@ package com.example.vartikasharma.carcrew.app_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.vartikasharma.carcrew.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.gson.Gson;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final String SERVER_HOST = "https://carcrew-project.firebaseio.com/";
+    private static final String URL = "http://ira-199298721.ap-south-1.elb.amazonaws.com/IRA/api/v1/enquiry_itemApi/?Enquiry_Source_ID=1";
+    private List<DataObject> dataObject = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReferenceFromUrl("gs://carcrew-project.appspot.com").child("download.json");
+        Log.i("storageRef," , storageRef.toString());
+
+        try {
+            fetchData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void fetchData() throws IOException, JSONException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(URL)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                Toast.makeText(getApplicationContext(), "Can't fetch data", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                String jsonData = response.body().string();
+                Log.i(LOG_TAG, jsonData);
+                Gson gson = new Gson();
+                if (response.isSuccessful()) {
+                    JSONObject json = null;
+                    JSONArray jsonArray;
+                    try {
+                        json = new JSONObject(jsonData);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        assert json != null;
+                        jsonArray  = json.getJSONArray("data");
+                        Log.i(LOG_TAG, "jsonArray," + jsonArray);
+                        Log.i(LOG_TAG, "length, " + jsonArray.length());
+                        for (int i = 0 ; i < jsonArray.length(); i++) {
+                            dataObject = Arrays.asList(gson.fromJson(jsonArray.toString(), DataObject[].class));
+                            Log.i(LOG_TAG, "first dataObject, " + dataObject.get(i).getCar_Name());
+                        }
+
+                        addValueToFirebase();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+                } else {
+                    Log.i(LOG_TAG, "response is not successful");
+                }
+
+            }
+        });
+    }
+
+    private void addValueToFirebase() {
+        final DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+       // DatabaseReference ref = database.getReference(SERVER_HOST);
+        Log.i(LOG_TAG, "ref, " + ref);
+        DatabaseReference usersRef = ref.child("data");
+        for (int i =0; i < dataObject.size(); i++) {
+            DatabaseReference reference = usersRef.push();
+            Log.i(LOG_TAG, "refernce, " + reference);
+            reference.setValue(dataObject.get(i));
+        }
+        usersRef.setValue(dataObject);
     }
 }
---
+++ app/src/main/java/com/example/vartikasharma/carcrew/app_1/DataObject.java
@@ -0,0 +1,149 @@
+package com.example.vartikasharma.carcrew.app_1;
+
+
+public class DataObject {
+    private String Garage_Name;
+    private int Quantity_In_Stock;
+    private String Part_Name;
+    private double Part_MRP;
+    private String Enquiry_Item_Carcrew_ID;
+    private String Brand_Name;
+    private String Car_Name;
+    private int Part_Carcrew_ID;
+    private int Enquiry_Item_Status;
+    private double Final_Price;
+    private double Part_CostPrice;
+    private int Garage_ID;
+    private int Part_Flag;
+    private String Created_At;
+    private int Enquiry_Item_ID;
+    private int Quantity;
+
+    public String getGarage_Name() {
+        return Garage_Name;
+    }
+
+    public void setGarage_Name(String garage_Name) {
+        Garage_Name = garage_Name;
+    }
+
+    public int getQuantity_In_Stock() {
+        return Quantity_In_Stock;
+    }
+
+    public void setQuantity_In_Stock(int quantity_In_Stock) {
+        Quantity_In_Stock = quantity_In_Stock;
+    }
+
+    public String getPart_Name() {
+        return Part_Name;
+    }
+
+    public void setPart_Name(String part_Name) {
+        Part_Name = part_Name;
+    }
+
+    public double getPart_MRP() {
+        return Part_MRP;
+    }
+
+    public void setPart_MRP(double part_MRP) {
+        Part_MRP = part_MRP;
+    }
+
+    public String getEnquiry_Item_Carcrew_ID() {
+        return Enquiry_Item_Carcrew_ID;
+    }
+
+    public void setEnquiry_Item_Carcrew_ID(String enquiry_Item_Carcrew_ID) {
+        Enquiry_Item_Carcrew_ID = enquiry_Item_Carcrew_ID;
+    }
+
+    public String getBrand_Name() {
+        return Brand_Name;
+    }
+
+    public void setBrand_Name(String brand_Name) {
+        Brand_Name = brand_Name;
+    }
+
+    public String getCar_Name() {
+        return Car_Name;
+    }
+
+    public void setCar_Name(String car_Name) {
+        Car_Name = car_Name;
+    }
+
+    public int getPart_Carcrew_ID() {
+        return Part_Carcrew_ID;
+    }
+
+    public void setPart_Carcrew_ID(int part_Carcrew_ID) {
+        Part_Carcrew_ID = part_Carcrew_ID;
+    }
+
+    public int getEnquiry_Item_Status() {
+        return Enquiry_Item_Status;
+    }
+
+    public void setEnquiry_Item_Status(int enquiry_Item_Status) {
+        Enquiry_Item_Status = enquiry_Item_Status;
+    }
+
+    public double getFinal_Price() {
+        return Final_Price;
+    }
+
+    public void setFinal_Price(double final_Price) {
+        Final_Price = final_Price;
+    }
+
+    public double getPart_CostPrice() {
+        return Part_CostPrice;
+    }
+
+    public void setPart_CostPrice(double part_CostPrice) {
+        Part_CostPrice = part_CostPrice;
+    }
+
+    public int getGarage_ID() {
+        return Garage_ID;
+    }
+
+    public void setGarage_ID(int garage_ID) {
+        Garage_ID = garage_ID;
+    }
+
+    public int getPart_Flag() {
+        return Part_Flag;
+    }
+
+    public void setPart_Flag(int part_Flag) {
+        Part_Flag = part_Flag;
+    }
+
+    public String getCreated_At() {
+        return Created_At;
+    }
+
+    public void setCreated_At(String created_At) {
+        Created_At = created_At;
+    }
+
+    public int getEnquiry_Item_ID() {
+        return Enquiry_Item_ID;
+    }
+
+    public void setEnquiry_Item_ID(int enquiry_Item_ID) {
+        Enquiry_Item_ID = enquiry_Item_ID;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        Quantity = quantity;
+    }
+}
